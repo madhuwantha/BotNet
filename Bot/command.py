594dae012a0b2c38a7b5f7f49b289750f8a1780f
madhuwantha <eng.madhuwanthapriyashan@gmail.com>
2021-01-31 18:58:58 +0530

    ex

--- Bot/command.py
+++ Bot/command.py
@@ -7,10 +7,14 @@ port = 7000
 bot = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 bot.connect((server, port))
 
-while True:
-    resp = bot.recv(2040).decode("UTF-8")
+try:
+    while True:
+        resp = bot.recv(2040).decode("UTF-8")
 
-    if resp.find('START') != -1:
-        print("Enter the command for bonnet")
-        cmd = input()
-        bot.send(bytes('CMD :' + cmd + '\r\n', "UTF-8"))
+        if resp.find('START') != -1:
+            print("Enter the command for bonnet")
+            cmd = input()
+            bot.send(bytes('CMD :' + cmd + '\r\n', "UTF-8"))
+except KeyboardInterrupt:
+    print("Press Ctrl-C to terminate while statement")
+    bot.close()
